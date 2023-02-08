06f2653bc4e62faa8eea815e891880aab173fc87
TheDoniki <h18donme@du.se>
2023-02-08 08:46:55 +0100

    Folders and files rearanged due to an error occuring when running project.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,3 @@
-///////////// https://github.com/facebook/create-react-app
-///////////// https://www.tatvasoft.com/blog/reactjs-best-practices/
-///////// https://www.mongodb.com/try/download/community
-//////////  https://create-react-app.dev/
-//// https://reactjs.org/docs/getting-started.html
-
-import React from "react";
-import ReactDOM from "react-dom/client";
-import "./index.css";
-import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
