462b394c7a53df786974274b7bd5018c333408ee
TheDoniki <h18donme@du.se>
2023-02-08 09:01:58 +0100

    Final changes in folder and file structure. This version 100% works and is deployable.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
@@ -9,4 +14,9 @@ root.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+
+///////////// https://github.com/facebook/create-react-app
+///////////// https://www.tatvasoft.com/blog/reactjs-best-practices/
+///////// https://www.mongodb.com/try/download/community
+//////////  https://create-react-app.dev/
+//// https://reactjs.org/docs/getting-started.html
