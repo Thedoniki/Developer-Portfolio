a0188c4541ab583da25d8bcbaf24d99916341d4d
TheDoniki <h18donme@du.se>
2023-02-12 20:09:00 +0100

    firebase connected, jsconfig file changed due to error.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,28 @@
 import logo from "./logo.svg";
 import "./App.css";
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDlIsRxOJ-aMXLTCFk1YB2yeDWvQZsOor8",
+  authDomain: "developer-portfolio-97fcc.firebaseapp.com",
+  projectId: "developer-portfolio-97fcc",
+  storageBucket: "developer-portfolio-97fcc.appspot.com",
+  messagingSenderId: "12702120675",
+  appId: "1:12702120675:web:f320a9e9acdc18a6b9e3d6",
+  measurementId: "G-H6CP4EKDJW"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+
+
 
 function App() {
   return (
